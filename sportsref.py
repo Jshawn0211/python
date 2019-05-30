1d075e17e0e66529d2969ed74df2cb6c999c8a39
jshawn0211 <jshawn0211@gmail.com>
2019-05-30 15:20:32 -0700

    added another module

--- sportsref.py
+++ sportsref.py
@@ -13,3 +13,13 @@ from sportsreference.mlb.boxscore import Boxscores
 
 games_today = Boxscores(datetime.today())
 print(games_today.games)  # Prints a dictionary of all matchups for today
+
+print("-------------------------------------")
+
+from sportsreference.mlb.roster import Roster
+
+dodgers = Roster('LAD')
+for player in dodgers.players:
+    # Prints the name of all players who played for the Dodgers in the most
+    # recent season.
+    print(player.name)
