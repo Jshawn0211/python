b60d5a850c1d66bf68c66812a2b0f514ba004437
jshawn0211 <jshawn0211@gmail.com>
2019-05-31 15:44:05 -0700

    scraping is working

--- scrape_test.py
+++ scrape_test.py
@@ -1,17 +1,12 @@
+#!/usr/bin/python3
+
 # A simple script to scrape a web site and get some info about todays MLB games
 
 #import the modules we are going to need
 import requests
 from bs4 import BeautifulSoup
 
-# We will start by scraping the daily matchups from http://www.baseballpress.com/lineups
-# First we set our website url
-url = "http://www.baseballpress.com/lineups"
+response = requests.get('https://www.rotowire.com/baseball/daily-lineups.php')
 
-# Read the source code of our webpage and create a BeautifulSoup Object so we can 
-# parse it and pull the data we want
-r = requests.get(url)
-data = r.text
-soup = BeautifulSoup(data)
-
-print(soup)
+print(response.text)
+print(response.headers)
