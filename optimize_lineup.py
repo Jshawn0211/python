a3d7b9cdf7c3624679bf3c35c6a735eb45375a16
jshawn0211 <jshawn0211@gmail.com>
2019-05-31 14:05:05 -0700

    updated optimizer logic to work!

--- optimize_lineup.py
+++ optimize_lineup.py
@@ -3,6 +3,6 @@
 from pydfs_lineup_optimizer import Site, Sport, get_optimizer
 
 optimizer = get_optimizer(Site.DRAFTKINGS, Sport.BASEBALL)
-optimizer.load_players_from_csv("DKSalaries.csv")
+optimizer.load_players_from_csv("05_31_mlb_night.csv")
 for lineup in optimizer.optimize(2):
     print(lineup)
